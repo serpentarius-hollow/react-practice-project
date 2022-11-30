6b79786929e8feb2f604e6f371256a9b89407ffe
Fridho Kesuma Pratama <fridho.pratama@ctcorpdigital.com>
2022-11-30 13:36:01 +0700

    Adding a UsersList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 const App = () => {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
